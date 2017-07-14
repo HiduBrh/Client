e8f88295df847a68421bb2f1a6f01b51bc6e202a
idu <mhamed.bourouh@gmail.com>
2017-07-14 18:46:05 +0200

    Initial commit

---
+++ main.c
@@ -0,0 +1,95 @@
+#include<stdio.h>
+#include<string.h>
+#include<sys/socket.h>
+#include<arpa/inet.h>
+#include <unistd.h>
+#define SERVER_PORT 50000
+#define SERVER_REPLY_SIZE 1000000
+#define QUIT_COMMANDE "QUIT"
+#define WANT_COMMANDE "WANT"
+#define NEED_COMMANDE "NEED"
+#define NOSUCHFILE "NOSUCHFILE"
+
+int main(int argc , char *argv[])
+{
+    int sock;
+    struct sockaddr_in server;
+    char criteres[200] , server_reply[SERVER_REPLY_SIZE], toSend[210];
+
+    sock = socket(AF_INET , SOCK_STREAM , 0);
+    if (sock == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons(SERVER_PORT);
+
+    //Connect to remote server
+    if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        perror("connect failed. Error");
+        return 1;
+    }
+
+    puts("Connected\n");
+
+    //keep communicating with server
+    while(1)
+    {
+        printf("saisissez les criteres de recherche [C1,C2,C3] : ");
+        scanf("%s" , criteres);
+        if(strcmp(QUIT_COMMANDE, criteres)==0) {
+            close(sock);
+            return 0;
+        }
+        //Send some data
+        strncpy(toSend,WANT_COMMANDE,strlen(WANT_COMMANDE));
+        strcat(toSend,criteres);
+        if( send(sock , toSend , strlen(toSend) , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+
+        //Receive a reply from the server
+        if( recv(sock , server_reply , SERVER_REPLY_SIZE , 0) < 0)
+        {
+            puts("recv failed");
+            break;
+        }
+
+        puts(server_reply);
+        if(strstr(server_reply, NOSUCHFILE) != NULL)
+            continue;
+        memset(server_reply, 0, sizeof(server_reply));
+
+        printf("saisissez l'id du fichier a recuperer : ");
+        scanf("%s" , criteres);
+        if(strcmp(QUIT_COMMANDE, criteres)==0) {
+            close(sock);
+            return 0;
+        }
+        //Send some data
+        strncpy(toSend,NEED_COMMANDE,strlen(NEED_COMMANDE));
+        strcat(toSend,criteres);
+        if( send(sock , toSend , strlen(toSend) , 0) < 0)
+        {
+            puts("Send failed");
+            return 1;
+        }
+        //Receive a reply from the server
+        if( recv(sock , server_reply , SERVER_REPLY_SIZE , 0) < 0)
+        {
+            puts("recv failed");
+            break;
+        }
+        puts(server_reply);
+        memset(server_reply, 0, sizeof(server_reply));
+    }
+
+    close(sock);
+    return 0;
+}
